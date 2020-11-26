f8b9a6d1ab9763e3b98b3dcba151b36a551ae6eb
Cykl0pS <miroyavuz@gmail.com>
2020-11-26 10:22:27 +0100

    WebAPI erstellt

---
+++ Chinook.WebAPI/Controllers/MarketingStatisticsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace Chinook.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MarketingStatisticsController : ControllerBase
+    {
+        [HttpGet("api/[controller]/ArtistStatistics")]
+        public IEnumerable<Contracts.Report.Marketing.IArtistStatistic> GetArtistStatistics()
+        {
+            return Report.MarketingReports.GetArtistStatistics();
+        }
+
+        [HttpGet("api/[controller]/CustomerInfo")]
+        public IEnumerable<Contracts.Report.Marketing.ICustomersInfo> GetCustomerInfo()
+        {
+            return (IEnumerable<Contracts.Report.Marketing.ICustomersInfo>)Report.MarketingReports.GetCustomersInfo();
+        }
+    }
+}
